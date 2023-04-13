bad1081a05d325d7063d1bbba201a8b6e9708121
masafumi yamamoto <ureuzy@gmail.com>
2023-04-13 13:51:23 +0900

    add org policy cloudfunctions.allowedIngressSettings

--- org/policies.tf
+++ org/policies.tf
@@ -31,3 +31,20 @@ module "restrict_authorized_networks" {
     }
   ]
 }
+
+module "allowed_ingress_settings" {
+  source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+
+  policy_root    = "organization"
+  policy_root_id = data.google_organization.ureuzy.org_id
+  constraint     = "constraints/cloudfunctions.allowedIngressSettings"
+  policy_type    = "list"
+  rules = [
+    {
+      enforcement = true
+      allow       = ["ALLOW_INTERNAL_AND_GCLB"]
+      deny        = []
+      conditions  = []
+    }
+  ]
+}
