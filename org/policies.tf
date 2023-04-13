5d03b7d05cdc966f7ed173b7f9b0b24c00b6c4ad
masafumi yamamoto <ureuzy@gmail.com>
2023-04-13 13:38:15 +0900

    add org policy sql restrict authorized networks

--- org/policies.tf
+++ org/policies.tf
@@ -1,4 +1,4 @@
-module "org-policy" {
+module "allowed_policy_member_domains" {
   source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
 
   policy_root    = "organization"
@@ -9,6 +9,23 @@ module "org-policy" {
     {
       enforcement = true
       allow       = var.allowed_policy_member_domains
+      deny        = []
+      conditions  = []
+    }
+  ]
+}
+
+module "restrict_authorized_networks" {
+  source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+
+  policy_root    = "organization"
+  policy_root_id = data.google_organization.ureuzy.org_id
+  constraint     = "constraints/sql.restrictAuthorizedNetworks"
+  policy_type    = "boolean"
+  rules = [
+    {
+      enforcement = true
+      allow       = []
       deny        = []
       conditions  = []
     }
