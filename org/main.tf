98dfbb761857e9ad19a6770aa92208c26f8a416c
masafumi yamamoto <ureuzy@gmail.com>
2023-05-10 11:34:15 +0900

    Revert "add policy"
    This reverts commit 4357a92bb90822bc916c24345e70eda24d487d35.

--- org/main.tf
+++ org/main.tf
@@ -1,8 +1,3 @@
 data "google_organization" "ureuzy" {
   domain = "ureuzy.io"
 }
-
-data "google_folder" "sample" {
-  folder              = "folders/1079656404947"
-  lookup_organization = true
-}
--- org/policies.tf
+++ org/policies.tf
@@ -1,19 +1,19 @@
-#module "allowed_policy_member_domains" {
-#  source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
-#
-#  policy_root    = "organization"
-#  policy_root_id = data.google_organization.ureuzy.org_id
-#  constraint     = "constraints/iam.allowedPolicyMemberDomains"
-#  policy_type    = "list"
-#  rules = [
-#    {
-#      enforcement = true
-#      allow       = var.allowed_policy_member_domains
-#      deny        = []
-#      conditions  = []
-#    }
-#  ]
-#}
+module "allowed_policy_member_domains" {
+  source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+
+  policy_root    = "organization"
+  policy_root_id = data.google_organization.ureuzy.org_id
+  constraint     = "constraints/iam.allowedPolicyMemberDomains"
+  policy_type    = "list"
+  rules = [
+    {
+      enforcement = true
+      allow       = var.allowed_policy_member_domains
+      deny        = []
+      conditions  = []
+    }
+  ]
+}
 
 module "restrict_authorized_networks" {
   source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
@@ -49,83 +49,31 @@ module "allowed_ingress_settings" {
   ]
 }
 
-module "skip_default_network_creation" {
+module "restricting_resource_for_system_project" {
   source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
   version = "5.2.2"
 
   policy_root    = "organization"
   policy_root_id = data.google_organization.ureuzy.org_id
-  constraint     = "constraints/compute.skipDefaultNetworkCreation"
-  policy_type    = "boolean"
+  constraint     = "constraints/gcp.restrictServiceUsage"
+  policy_type    = "list"
   rules = [
     {
       enforcement = true
-      allow       = []
-      deny        = []
-      conditions  = []
+      allow = [
+        "clouddebugger.googleapis.com",
+        "cloudkms.googleapis.com",
+        "cloudtrace.googleapis.com",
+        "datastore.googleapis.com",
+        "storage.googleapis.com"
+      ]
+      deny = []
+      conditions = [{
+        description = "Limit resources used by system projects"
+        expression  = "resource.matchTag('948429943190/project', 'system')"
+        location    = ""
+        title       = "Limit resources used by system projects"
+      }]
     }
   ]
 }
-
-module "automatic_iam_grants_default_service_accounts" {
-  source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
-  version = "5.2.2"
-
-  policy_root    = "organization"
-  policy_root_id = data.google_organization.ureuzy.org_id
-  constraint     = "constraints/iam.automaticIamGrantsForDefaultServiceAccounts"
-  policy_type    = "boolean"
-  rules = [
-    {
-      enforcement = true
-      allow       = []
-      deny        = []
-      conditions  = []
-    }
-  ]
-}
-
-#module "restrict_resources_to_system_project" {
-#  source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
-#  version = "5.2.2"
-#
-#  policy_root    = "organization"
-#  policy_root_id = data.google_organization.ureuzy.org_id
-#  constraint     = "constraints/gcp.restrictServiceUsage"
-#  policy_type    = "list"
-#  rules = [
-#    {
-#      enforcement = true
-#      allow = [
-#        "cloudkms.googleapis.com",
-#        "storage.googleapis.com"
-#      ]
-#      deny = []
-#      conditions = [{
-#        description = "Limit resources used by system projects"
-#        expression  = "resource.matchTag('${data.google_organization.ureuzy.org_id}/project', 'system')"
-#        location    = ""
-#        title       = "Limit resources used by system projects"
-#      }]
-#    },
-#  ]
-#}
-#
-#module "restrict_resources_to_system_project_folder" {
-#  source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
-#  version = "5.2.2"
-#
-#  policy_root     = "organization"
-#  policy_root_id  = data.google_organization.ureuzy.org_id
-#  constraint      = "constraints/gcp.restrictServiceUsage"
-#  policy_type     = "list"
-#  exclude_folders = [data.google_folder.sample.folder_id]
-#  rules = [
-#    {
-#      enforcement = false
-#      allow       = []
-#      deny        = ["bigqueryreservation.googleapis.com"]
-#      conditions  = []
-#    }
-#  ]
-#}
