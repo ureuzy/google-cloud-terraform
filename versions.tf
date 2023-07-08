174b66f1c0bec61f7fc72ab0e1ea464d7fcb7c28
masafumi yamamoto <ureuzy@gmail.com>
2023-07-08 21:00:49 +0900

    update renovate

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "1.4.4"
+  required_version = "~> 1.5.0"
   required_providers {
-    google      = "4.60.2"
-    google-beta = "4.60.2"
+    google      = "4.72.1"
+    google-beta = "4.72.1"
     sops = {
       source  = "carlpett/sops"
       version = "0.7.2"
