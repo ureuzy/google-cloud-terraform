14155e7628154749a6ae8536503d56bde2f2f90e
masafumi yamamoto <ureuzy@gmail.com>
2023-10-02 13:48:31 +0900

    delete policy

--- policies.tf
+++ policies.tf
@@ -15,29 +15,29 @@
 #  ]
 #}
 
-module "allowed_service_usage" {
-  source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
-
-  policy_root    = "project"
-  policy_root_id = google_project.org_system.project_id
-  constraint     = "constraints/gcp.restrictServiceUsage"
-  policy_type    = "list"
-  rules = [
-    {
-      enforcement = true
-      allow       = [
-        "cloudkms.googleapis.com",
-        "storage.googleapis.com",
-        "cloudbuild.googleapis.com",
-        "cloudfunctions.googleapis.com",
-        "pubsub.googleapis.com",
-        "run.googleapis.com",
-        "eventarc.googleapis.com",
-        "artifactregistry.googleapis.com",
-        "secretmanager.googleapis.com"
-      ]
-      deny        = []
-      conditions  = []
-    }
-  ]
-}
+#module "allowed_service_usage" {
+#  source = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+#
+#  policy_root    = "project"
+#  policy_root_id = google_project.org_system.project_id
+#  constraint     = "constraints/gcp.restrictServiceUsage"
+#  policy_type    = "list"
+#  rules = [
+#    {
+#      enforcement = true
+#      allow       = [
+#        "cloudkms.googleapis.com",
+#        "storage.googleapis.com",
+#        "cloudbuild.googleapis.com",
+#        "cloudfunctions.googleapis.com",
+#        "pubsub.googleapis.com",
+#        "run.googleapis.com",
+#        "eventarc.googleapis.com",
+#        "artifactregistry.googleapis.com",
+#        "secretmanager.googleapis.com"
+#      ]
+#      deny        = []
+#      conditions  = []
+#    }
+#  ]
+#}
