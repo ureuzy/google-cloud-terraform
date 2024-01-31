0c94cb5f75d13f3aef007e3b4f164614977f3b7c
masafumi yamamoto <ureuzy@gmail.com>
2024-01-31 11:20:43 +0900

    Add policy

--- policies.tf
+++ policies.tf
@@ -107,3 +107,21 @@ module "disable_automatic_iam_grants" {
     }
   ]
 }
+
+module "test" {
+  source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+  version = "5.2.2"
+
+  policy_root    = "organization"
+  policy_root_id = data.google_organization.ureuzy.org_id
+  constraint     = "constraints/iam.disableServiceAccountKeyCreation"
+  policy_type    = "boolean"
+  rules          = [
+    {
+      enforcement = true
+      allow       = []
+      deny        = []
+      conditions  = []
+    }
+  ]
+}
