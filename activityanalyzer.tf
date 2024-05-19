aba9451918d95d8cf8cf3eb7639e6e9f5044fca4
masafumi yamamoto <ureuzy@gmail.com>
2024-05-20 04:14:43 +0900

    fix scheduler for activity analyzer

--- activityanalyzer.tf
+++ activityanalyzer.tf
@@ -3,7 +3,7 @@ resource "google_cloud_scheduler_job" "activity_analyzer" {
   description = "Analyze unused service account"
   project     = google_project.org_system.project_id
   region      = "asia-northeast1"
-  schedule    = "0 0 * * *"
+  schedule    = "0 0 * * 1"
   time_zone   = "Asia/Tokyo"
   pubsub_target {
     topic_name = google_pubsub_topic.activity_analyzer.id
