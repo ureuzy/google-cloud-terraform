30a6d3a25b4fad3288f0962c6ee6f811906921b5
masafumi yamamoto <ureuzy@gmail.com>
2025-09-27 23:20:44 +0900

    add bucket

--- storage.tf
+++ storage.tf
@@ -5,3 +5,11 @@ resource "google_storage_bucket" "photos" {
   public_access_prevention = "enforced"
   force_destroy            = false
 }
+
+resource "google_storage_bucket" "test" {
+  name                     = "ureuzy-test"
+  location                 = "asia-northeast1"
+  project                  = google_project.org_system.project_id
+  public_access_prevention = "enforced"
+  force_destroy            = false
+}
