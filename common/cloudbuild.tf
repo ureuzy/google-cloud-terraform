a33c67e6fda7080286d9d8f5c0d54713c5443416
masafumi yamamoto <ureuzy@gmail.com>
2026-01-31 02:42:15 +0900

    Migrate Cloud Build trigger to v2 with Repository API

--- common/cloudbuild.tf
+++ common/cloudbuild.tf
@@ -5,24 +5,64 @@ resource "google_artifact_registry_repository" "common" {
   format        = "DOCKER"
 }
 
-resource "google_cloudbuild_trigger" "mitene_downloader" {
-  project     = data.google_project.main.project_id
-  location    = "global"
-  name        = "mitene-downloader"
-  description = "Build and Deploy mitene-downloader to Cloud Run Job"
+# 1. GitHub PAT 保存用のシークレット
+resource "google_secret_manager_secret" "github_pat" {
+  project   = data.google_project.main.project_id
+  secret_id = "github-pat"
+  replication {
+    auto {}
+  }
+}
 
-  github {
-    owner = "ureuzy"
-    name  = "cloud_functions"
+# 2. Cloud Build サービスエージェントにシークレットの読み取り権限を付与
+resource "google_secret_manager_secret_iam_member" "cloudbuild_secrets_accessor" {
+  project   = data.google_project.main.project_id
+  secret_id = google_secret_manager_secret.github_pat.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:service-${data.google_project.main.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
+}
+
+# 3. Cloud Build 第2世代の接続 (Connection)
+resource "google_cloudbuildv2_connection" "github_conn" {
+  project  = data.google_project.main.project_id
+  location = "asia-northeast1"
+  name     = "github-conn"
+
+  github_config {
+    app_installation_id = var.github_app_installation_id
+
+    authorizer_credential {
+      oauth_token_secret_version = "${google_secret_manager_secret.github_pat.id}/versions/latest"
+    }
+  }
+
+  depends_on = [google_secret_manager_secret_iam_member.cloudbuild_secrets_accessor]
+}
+
+# 4. リポジトリの登録
+resource "google_cloudbuildv2_repository" "cloud_functions" {
+  project           = data.google_project.main.project_id
+  location          = "asia-northeast1"
+  name              = "cloud-functions"
+  parent_connection = google_cloudbuildv2_connection.github_conn.name
+  remote_uri        = "https://github.com/ureuzy/cloud_functions.git"
+}
+
+# 5. トリガーの修正 (第2世代を参照)
+resource "google_cloudbuild_trigger" "mitene_downloader" {
+  project  = data.google_project.main.project_id
+  location = "asia-northeast1"
+  name     = "mitene-downloader"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.cloud_functions.id
     push {
       branch = "^main$"
     }
   }
 
-  # Once the trigger is created successfully, we can re-add included_files
   included_files = ["mitene-downloader/**"]
-
-  filename = "mitene-downloader/cloudbuild.yaml"
+  filename       = "mitene-downloader/cloudbuild.yaml"
 
   substitutions = {
     _REGION        = "asia-northeast1"
--- common/variables.tf
+++ common/variables.tf
@@ -1,3 +1,4 @@
-variable "TFC_GCP_WORKLOAD_PROVIDER_NAME" {}
-variable "TFC_GCP_PROVIDER_AUTH" {}
-variable "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL" {}
+variable "github_app_installation_id" {
+  type        = number
+  description = "The installation ID of the Google Cloud Build GitHub App."
+}
