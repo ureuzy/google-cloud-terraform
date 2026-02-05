fa8c4b780f71461b4e9767e74e962fb32900bcb4
masafumi yamamoto <ureuzy@gmail.com>
2026-02-06 01:29:08 +0900

    cleanup: remove cloudrun.tf and use data source for pubsub push endpoint

--- common/pubsub.tf
+++ common/pubsub.tf
@@ -3,12 +3,18 @@ resource "google_pubsub_topic" "audit_alert" {
   project = data.google_project.main.project_id
 }
 
+data "google_cloud_run_v2_service" "audit_alert" {
+  name     = "audit-alert"
+  location = "asia-northeast1"
+  project  = data.google_project.main.project_id
+}
+
 resource "google_pubsub_subscription" "audit_alert" {
   name  = "audit-alert"
   topic = google_pubsub_topic.audit_alert.name
 
   push_config {
-    push_endpoint = "${google_cloud_run_v2_service.audit_alert.uri}/"
+    push_endpoint = "${data.google_cloud_run_v2_service.audit_alert.uri}/"
     oidc_token {
       service_account_email = google_service_account.audit_alert.email
     }
