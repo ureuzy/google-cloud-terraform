4ab3d41e2be13a6a87b356fe593bcebd9373ce66
masafumi yamamoto <ureuzy@gmail.com>
2026-02-12 02:11:27 +0900

    add: cleanup policies to artifact registry

--- common/artifactregistry.tf
+++ common/artifactregistry.tf
@@ -2,4 +2,13 @@ resource "google_artifact_registry_repository" "common" {
   location      = "asia-northeast1"
   repository_id = "common"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest-5"
+    action = "KEEP"
+
+    most_recent_versions {
+      keep_count = 5
+    }
+  }
 }
