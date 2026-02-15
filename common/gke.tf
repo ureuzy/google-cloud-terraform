7df6137d2a4d5eae87f5970a580bd5265b1aad1e
masafumi yamamoto <ureuzy@gmail.com>
2026-02-16 03:32:56 +0900

    Switch GKE from Autopilot to Standard with e2-small Spot VMs and autoscaling

--- common/gke.tf
+++ common/gke.tf
@@ -1,18 +1,33 @@
 resource "google_container_cluster" "main" {
   name     = "common"
-  location = "asia-northeast1"
+  location = "asia-northeast1-a"
 
-  enable_autopilot = true
-
-  cluster_autoscaling {
-    auto_provisioning_defaults {
-      service_account = google_service_account.service_accounts["gke-common"].email
-    }
-  }
+  initial_node_count       = 1
+  remove_default_node_pool = true
 
   release_channel {
     channel = "RAPID"
   }
 
   deletion_protection = false
+}
+
+resource "google_container_node_pool" "default" {
+  name     = "default"
+  cluster  = google_container_cluster.main.name
+  location = google_container_cluster.main.location
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    machine_type    = "e2-small"
+    spot            = true
+    disk_size_gb    = 30
+    disk_type       = "pd-standard"
+    service_account = google_service_account.service_accounts["gke-common"].email
+    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 }
